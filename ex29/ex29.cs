eda9ee67b5b609aa11ce034f65cc6fa1fa306d0f
Vlad A <seralesin@mail.ru>
2023-01-15 16:26:38 +0300

    сделал 3ю задачу, но не совсем понял как можно по-другому сделать вывод

---
+++ ex29/ex29.cs
@@ -0,0 +1,34 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+
+все 8 элементов должны получить за один Console.ReadLine();
+подсказка: использовать потом метод string.Split(); */
+
+string GetMas(string message)
+{
+    Console.WriteLine(message);
+    string result = Console.ReadLine();
+    return result;
+}
+
+
+string massive = GetMas ("введите 8 чисел через запятую");
+massive = massive.Replace(" ", "");
+string[] array = massive.Split(',');
+
+ for (int i=0; i<=array.Length-1; i++)
+{ 
+    Console.Write(array[i]);
+    if (i!=array.Length-1)
+    {
+        Console.Write(@", ");        
+    }
+    else
+    {
+        break;    
+    }
+}
+
